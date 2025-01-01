788ada24885e78a624d3397a09e269b1a542a557
Pranit Patil <pranitvp2408@gmail.com>
2025-01-01 15:47:15 +0530

    Add files via upload

---
+++ 1.hello.java
@@ -0,0 +1,12 @@
+
+    class hello {
+      
+ 
+     
+        public static void main(String[] var0) {
+           System.out.print("Hello world");
+        }
+     }
+     
+    
+
---
+++ 2.Basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    
+    public static void main(String a[])
+    {
+       byte b1 = 5,b2 = 6;
+       byte b3 =(byte)(b1+b2);
+       System.out.println(b3);
+    }
+    
+}
