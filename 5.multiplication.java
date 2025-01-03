84050169c312f928d084a7878271439fd29ac213
Pranit Patil <pranitvp2408@gmail.com>
2025-01-03 16:28:48 +0530

    Add files via upload

---
+++ 5.multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    
+    public static void main(String a[])
+    {
+       int num1=5;
+       int num2=2;
+
+       int result = num1 * num2;
+       System.out.println(result);
+    }
+    
+}
---
+++ 6.Division.java
@@ -0,0 +1,11 @@
+public class division {
+    public static void main(String a[])
+    {
+       int num1=5;
+       int num2=2;
+
+       int result = num1 /num2;
+       System.out.println(result);
+    }
+    
+}
