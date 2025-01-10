ca969fce54755d95e163ddd809286d0652833cff
Pranit Patil <pranitvp2408@gmail.com>
2025-01-10 12:02:13 +0530

    Add files via upload

---
+++ 10.Logicalop.java
@@ -0,0 +1,15 @@
+public class logicalop {
+    
+    public static void main( String []args)
+    {
+        int x = 6;
+        int y = 5;
+        int a = 4;
+        int b = 5;
+
+        boolean result = x>y || a>b;
+
+        System.out.println(result);
+    }
+    
+}
---
+++ 12.Conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    
+    public static void main( String []args)
+    {
+        int x = 18;
+        
+        if(x<10)
+            System.out.println("hello");
+        else
+            System.out.println("Bye");
+    }
+
+    
+}
